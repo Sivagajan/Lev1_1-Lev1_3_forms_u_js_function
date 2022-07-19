144ca507cf7cbe3c6951f6175608d0ec33d01c21
Sivagajan <sivagajan@gmail.com>
2022-07-19 14:15:42 +0200

    tasks 1_1 - 1_3

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+console.log('funzt')
+
+/* let numberInput = document.getElementById('number')
+let resultTag = document.getElementById('result')
+
+function double(){
+    console.log('hallo')
+    let result = Number(numberInput.value) *2
+    resultTag.innerHTML= result
+}
+ */
+
+/*---------------------------Lev1_2-----------------------*/
+
+/* let yearTag = document.getElementById('year')
+let ageTag = document.getElementById('age')
+
+function calculateAge(){
+    console.log('Hallo')
+    let age = 2022 - Number(yearTag.value)
+    ageTag.innerHTML = age
+} */
+
+/*---------------------------Lev1_3-----------------------*/
+
+/* let yearTag1 = document.getElementById('year1')
+let yearTag2 = document.getElementById('year2')
+let ageDiffTag = document.getElementById('ageDiff')
+let difference
+
+function calculateDiff(){
+
+    console.log('Hallo')
+    let age1 = Number(yearTag1.value)
+    let age2 = Number(yearTag2.value)
+
+    if(age1 > age2){
+
+        difference = age1 - age2
+
+    } else if(age1 <= age2) {
+        
+        difference = age2 - age1
+    }
+
+    ageDiffTag.innerHTML = difference
+}
+ */
